b1c62cffd6a1df957e2b19edc0d205fe39f26554
Pascal-Onaho <Onahopascal@gmail.com>
2026-04-05 23:06:17 +0100

    updated

--- simple_games/damola/src/main.rs
+++ simple_games/damola/src/main.rs
@@ -1,5 +1,16 @@
 fn main() {
-    let s = "Hello"; // String literal(Stored on stack)
-    let p = String::from("Pascal"); // String (Stored on heap)
-    println!("Here are the words: {} {}", s, p);
+    let width: i32 = 40;
+    let height: i32 = 70;
+
+    let soln = area(width, height);
+
+    println!("{soln}");
+}
+
+fn area(width: i32, length: i32) -> i32 {
+
+    println!("The area of the shape is:");
+    let shape_area = width * length;
+    return shape_area
+
 }
